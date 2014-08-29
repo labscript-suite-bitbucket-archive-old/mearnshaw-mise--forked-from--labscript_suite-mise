6836e2d2a3a7b896554f36fcfd688e99d630f54e
Matt Earnshaw <matt@earnshaw.org.uk>
2014-08-29 16:37:58 +0100

    add setup files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+setup(
+        name = "mise",
+        include_package_data = True,
+        packages = find_packages(),
+        install_requires = ['zprocess']
+)
+
